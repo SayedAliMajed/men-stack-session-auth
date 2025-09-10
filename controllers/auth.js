096652c88b79f60dd5d5e6ef7c58eabf3d210b20
SayedAli <sayed.ali.majed@gmail.com>
2025-09-10 11:38:03 +0300

    Sign-up completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,53 @@
+const User = require("../models/user.js");
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
+
+
+router.get('/sign-up', (req,res) => {
+    res.render('auth/sign-up.ejs');
+});
+
+router.get('/sign-in', (req,res) => {
+res.render('auth/sign-in.ejs');
+});
+// Sign-up create new user
+
+router.post('/sign-up', async (req, res) => {
+const userInDatabase = await User.findOne({ username: req.body.username });
+
+if (userInDatabase) {
+    return res.send('Username or Password is invalid');
+}
+
+if (req.body.password !== req.body.confirmPassword) {
+    return res.send('Password and Confirm Password must match');
+}
+
+const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+req.body.password = hashedPassword;
+
+const user = await User.create(req.body);
+res.send(`Thanks for signing up ${user.username}`);
+
+});
+
+// Sign-in 
+
+router.post('/sign-in', async (req,res) =>{
+    const userInDatabase = await User.findOne({ username: req.body.username});
+    if (!userInDatabase) {
+        return res.send('Username or Password is invalid');
+    }
+
+    const validPassword = bcrypt.compareSync(
+        req.body.validPassword,
+        userInDatabase.password
+    );
+
+    if (!validPassword) {
+        return res.send('Username or Password is invalid');
+    }
+});
 module.exports = router;
