5f37d24518c85e95e289bcf4b06df2510ba0d1d2
SayedAli <sayed.ali.majed@gmail.com>
2025-09-10 17:00:49 +0300

    completed the auth

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require('express-session');
 const MongoStore = require("connect-mongo");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
+
+
 
 // Set the port from environment variable or default to 3000
 let port;
@@ -41,11 +45,11 @@ app.use(
     }),
   })
 );
-
+ app.use(passUserToView);
 // GET /pulic
 app.get("/", async (req, res) => {
   res.render('index.ejs', {
-    user: req.session.user,
+    //user: req.session.user,
   });
 });
 
@@ -59,6 +63,11 @@ app.get('/vip-lounge', (req,res) => {
     res.send('Sorry, no guest allowed');
   }
 });
+
+app.get("/vip-lounge", isSignedIn, (req, res) => {
+  res.send(`Welcome to the party ${req.session.user.username}.`);
+});
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
